d48dd0dd2e7a4f1e5a4e4b298ad186de5e866d75
KrieqqeR <burakcevizli@gmail.com>
2023-11-09 13:56:32 +0300

    Cors cozulur gibi oldu sifre 123 oluy ohep.

--- src/main/java/com/twitter/twitter/config/SecurityConfig.java
+++ src/main/java/com/twitter/twitter/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.twitter.twitter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -12,33 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
-/*@Configuration
-public class SecurityConfig {
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/profile/**").permitAll();
-                    auth.anyRequest().authenticated();
-                })
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-                .build();
-    }
-
-
-}
-*/
-
-
-
+import java.util.Arrays;
 
 //csrf => Cross Site Resource Forgery
 //Authentication(Giriş Yapabilme) vs Authorization(Yetkilendirme-Rol based)
@@ -70,9 +49,21 @@ public class SecurityConfig {
         return new ProviderManager(daoProvider);
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
     //JSF(Java Server Faces), Thymeleaf, JSP(Java Server Page)
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(Customizer.withDefaults());
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/profile/**").permitAll();
@@ -80,8 +71,10 @@ public class SecurityConfig {
                 })
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())  // Add this line to include CORS configuration
                 .build();
     }
+
 
 }
 
